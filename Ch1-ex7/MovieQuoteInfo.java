7a876048a0e7fa0cbec59bbe15641260c98307d2
Andres Succar <asuccarcuellar@ivytech.edu>
2026-09-02 12:50:53 -0400

    ch1-ex7sc

---
+++ Ch1-ex7/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+//Andres Succar
+//p.35 Exercise 7
+
+
+
+public class MovieQuoteInfo{
+    public static void main(String[] args) {
+System.out.println("Famous quote: \" Hasta la vista, baby!\"");
+System.out.println("Name of the movie:  Terminator 2");
+System.out.println("Name of the character: T-800 ");
+System.out.println("Year released: 1991 ");
+
+    }
+}
+ 
